69d33708f010e056ae56e839b26f214a1bb56836
Daisuke Murase <typester@cpan.org>
2024-11-11 21:25:04 -0800

    init ios project

---
+++ ios/Auth2/Auth2App.swift
@@ -0,0 +1,17 @@
+//
+//  Auth2App.swift
+//  Auth2
+//
+//  Created by Daisuke Murase on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Auth2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Auth2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Auth2
+//
+//  Created by Daisuke Murase on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
